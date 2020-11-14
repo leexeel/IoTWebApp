515e0d5d7d9e4e8592607738400db8f2584835bc
Liviu Strati <liviu.strati@hdt.ro>
2020-11-14 14:46:50 +0200

    db connet function

--- web/libs/dblib.php
+++ web/libs/dblib.php
@@ -1,7 +1,13 @@
 <?php
+../include/include.php
 
 function dbConnect() {
-    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
 }
 
 ?>
