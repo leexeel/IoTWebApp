82017a37db292a4fd6ccd0939bffaaba1d9540b6
Liviu Strati <liviu.strati@hdt.ro>
2020-11-15 10:10:34 +0200

    mysql problem solved

--- web/libs/dblib.php
+++ web/libs/dblib.php
@@ -1,12 +1,16 @@
 <?php
-include 'include.php';
+//include 'include.php';
 
 function dbConnect() {
-    $conn = new mysqli($servername, $username, $password, $dbname);
+  $dbServer = "192.168.10.66";
+  $dbUser = "iot_user";
+  $dbPassword = "init1234";
+  $dbDataBase = "iot";
+  $conn = new mysqli($dbServer, $dbUser, $dbPassword, $dbDataBase);
     // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    return $conn;
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  return $conn;
 }
 ?>
