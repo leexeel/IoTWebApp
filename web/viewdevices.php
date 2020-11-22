4ef0c4a2abe5eb3808e814ab24f8935cfca2581a
Liviu Strati <liviu.strati@hdt.ro>
2020-11-22 09:03:18 +0200

    css

--- web/viewdevices.php
+++ web/viewdevices.php
@@ -11,5 +11,21 @@ include "libs/functions.php";
 drawHeader();
 drawPage();
 
+$conn = dbConnect();
+
+$sql = "SELECT * FROM devices";
+$result = $conn->query($sql);
+echo "<div class=tableList>";
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["idDevice"].$row["dvName"].$row["dvLocation"].$row["sensNumber"].$row["devDescription"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+echo "</div>";
+$conn->close();
+
 drawFooter();
 ?>
