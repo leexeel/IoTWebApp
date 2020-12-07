242ecbf09f02d517a22477fae148a3524880e014
Liviu Strati <liviu.strati@hdt.ro>
2020-12-07 11:07:08 +0200

    .

--- mqtt/iotDevice.py
+++ mqtt/iotDevice.py
@@ -22,11 +22,28 @@ class iotDevice:
 
     def setTelemetryTopic(self):
         for sensor in enumerate(self.sensorsList):
-            telemetryTopic = self.baseTopicConf + "/telemetry/" + sensor[1]
+            telemetryTopic = "values/" + self.deviceID + "/telemetry/" + sensor[1]
+            #print(telemetryTopic)
             self.mqttClient.subscribe(telemetryTopic)
             
     def __configDeviceTelemetry(self):
         configString = "Config|"
         for sensor in enumerate(self.sensorsList):
             configString = configString + sensor[1] + "|"
+        configString = configString + "end"
         self.mqttClient.publish(self.baseTopicConf,configString)
+    
+    def procMessage(self,topic,message):
+        print("In obiect am primit mesajul:",message," pe topicul:",topic)
+        identDevice = topic.split("/")
+        if self.deviceID == identDevice[1]:
+            print("este pentru obiectul asta")
+            self.__messageTelemetry(self,identDevice[3],message)
+        else:
+            print("mesajul este pentru alt obiect")
+
+    def __messageTelemetry(self,sensor,message):
+        #identDevice = topic.split("/")
+        print("Mesajul:",message)
+        pass
+    
--- mqtt/mqtt-client.py
+++ mqtt/mqtt-client.py
@@ -1,12 +1,60 @@
 import paho.mqtt.client as mqtt
 import time
-from iotDevice import iotDevice
+#from iotDevice import iotDevice
+import uuid
 
 deviceList = []
 
+class iotDevice:
+    
+    def __init__(self,deviceID,sensors,deviceIP,mqttClient):
+        self.mqttClient = mqttClient
+        self.deviceID = deviceID
+        self.sensors = sensors
+        self.deviceIP = deviceIP
+        self.baseTopicConf = "events/"+deviceID
+        self.sensorsList = []
+        self.__setSensorID()
+        self.setTelemetryTopic()
+        self.__configDeviceTelemetry()
+    
+    def __setSensorID(self):
+        count = 1
+        while count <= self.sensors:
+            tempSensorId = self.deviceID + str(self.sensors) + str(count)
+            sensorUUID = uuid.uuid5(uuid.NAMESPACE_DNS,tempSensorId)
+            self.sensorsList.append(str(sensorUUID))
+            count = count+1
+
+    def setTelemetryTopic(self):
+        for sensor in enumerate(self.sensorsList):
+            telemetryTopic = "values/" + self.deviceID + "/telemetry/" + sensor[1]
+            #print(telemetryTopic)
+            self.mqttClient.subscribe(telemetryTopic)
+            
+    def __configDeviceTelemetry(self):
+        configString = "Config|"
+        for sensor in enumerate(self.sensorsList):
+            configString = configString + sensor[1] + "|"
+        configString = configString + "end"
+        self.mqttClient.publish(self.baseTopicConf,configString)
+    
+    def procMessage(self,topic,message):
+        print("In obiect am primit mesajul:",message," pe topicul:",topic)
+        identDevice = topic.split("/")
+        if self.deviceID == identDevice[1]:
+            print("este pentru obiectul asta",identDevice[1]," ",identDevice[3])
+            self.messageTelemetry(self,identDevice[3],message)
+        else:
+            print("mesajul este pentru alt obiect")
+
+    def messageTelemetry(self,sensor,message):
+        #identDevice = topic.split("/")
+        print("Mesajul:",message)
+
+
 ############
 def on_message(client, userdata, message):
-    print("message topic=",message.topic.encode('ascii', 'ignore'),"value=",str(message.payload.decode("utf-8")))
     if message.topic.encode('ascii', 'ignore') == "events/join":
         #print(str(message.payload.decode("utf-8")))
         deviceParameters=str(message.payload.decode("utf-8")).split('|')
@@ -17,23 +65,22 @@ def on_message(client, userdata, message):
         mqttClient.publish(pubThread,welcomeString)
         mqttClient.subscribe(pubThread)
         print("Lista este:",deviceList)
+    else:
+        #print("message topic=",message.topic.encode('ascii', 'ignore'),"value=",str(message.payload.decode("utf-8")))
+        for device in enumerate(deviceList):
+            print("**************:",device," ",device[1])
+            device[1].procMessage(message.topic.encode('ascii', 'ignore'),str(message.payload.decode("utf-8")))
 
 def on_publish(client,userdata,result):             #create function for callback
-    print("data published \n")
+    #print("data published \n")
     pass
 ########################################
 broker_address="192.168.10.66"
-
 print("creating new instance")
 mqttClient = mqtt.Client("ProcClients") #create new instance
 mqttClient.on_message=on_message #attach function to callback
 print("connecting to broker")
 mqttClient.connect(broker_address) #connect to broker
-print("Subscribing to topic","values/temperature")
-mqttClient.subscribe("values/temperature")
-print("Subscribing to topic","values/humidity")
-mqttClient.subscribe("values/humidity")
-print("Subscribing to topic","events/join")
 mqttClient.subscribe("events/join")
 mqttClient.on_publish = on_publish
 mqttClient.loop_forever() #start the loop
